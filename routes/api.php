71c416855ad936d4891292d36b49828ce72970f7
resakastepme <resa.komara.akbari@gmail.com>
2024-01-04 17:14:48 +0700

    ADD MAINTENANCE TO API ROUTE

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// ROUTE TO MAINTENANCE MODE
+Route::get('/maintenance-mode', function () {
+    Artisan::call('down');
+    return 'Maintenance mode is enabled';
+});
+
+// ROUTE TO DISABLE MAINTENANCE MODE
+Route::get('/disable-maintenance-mode', function () {
+    Artisan::call('up');
+    return 'Maintenance mode is disabled';
 });
